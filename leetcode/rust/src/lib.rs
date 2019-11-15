6d16a9742020960e8542a0cc2f36e4c7befe22e6
Chen Junda <ddadaal@outlook.com>
2019-11-15 22:17:53 +0800

    debug available on main fn

--- leetcode/rust/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![allow(dead_code)]
-mod q1;
-mod q2;
-mod q3;
-mod utils;
---
+++ leetcode/rust/src/main.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code)]
+mod utils;
+mod q1;
+mod q2;
+mod q3;
+
+use utils::Solution;
+
+fn main() {
+    Solution::length_of_longest_substring(String::from("123"));
+}
